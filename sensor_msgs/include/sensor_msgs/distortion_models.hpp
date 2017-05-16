a220fd1e30ea9a61caa5391cedb54c3f505cb90a
Chris Lalancette <clalancette@osrfoundation.org>
2017-05-16 12:26:24 -0400

    Add a comment saying where distortion_model.hpp originally came from.
    Signed-off-by: Chris Lalancette <clalancette@osrfoundation.org>

--- sensor_msgs/include/sensor_msgs/distortion_models.hpp
+++ sensor_msgs/include/sensor_msgs/distortion_models.hpp
@@ -31,6 +31,9 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+// This file is originally ported from ROS1:
+// https://github.com/ros/common_msgs/blob/jade-devel/sensor_msgs/include/sensor_msgs/distortion_models.h
+
 #ifndef SENSOR_MSGS__DISTORTION_MODELS_HPP_
 #define SENSOR_MSGS__DISTORTION_MODELS_HPP_
 
