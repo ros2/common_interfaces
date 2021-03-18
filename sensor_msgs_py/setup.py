c86cc19ba2ec255891e756da618b8d46d6030b44
Evan Flynn <6157095+flynneva@users.noreply.github.com>
2021-03-18 16:16:05 -0700

    Add sensor_msgs_py (#143)
    Backports #128 
    
    * Port of point_cloud2.py from ROS1 to ROS2. As seperate pkg. (#128)
    
    Signed-off-by: Sebastian Grans <sebastian.grans@ntnu.no>
    
    Co-authored-by: Sebastian Grans <sebastian.grans@ntnu.no>
    Signed-off-by: flynneva <evanflynn.msu@gmail.com>
    
    * align version number with rest of foxy packages
    
    Signed-off-by: flynneva <evanflynn.msu@gmail.com>
    
    Co-authored-by: Sebastian Grans <sebastian.grans@gmail.com>
    Co-authored-by: Sebastian Grans <sebastian.grans@ntnu.no>

---
+++ sensor_msgs_py/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = 'sensor_msgs_py'
+
+setup(
+    name=package_name,
+    version='2.0.3',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Michael Carroll',
+    maintainer_email='michael@openrobotics.org',
+    author='Sebastian Grans',
+    author_email='sebastian.grans@gmail.com',
+    description='A package for easy creation and reading of PointCloud2 messages in Python.',
+    license='BSD',
+    tests_require=['pytest'],
+)
