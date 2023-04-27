20347055c0f9e25ee8b95f6cabdc0c5a10b417ef
Yadunund <yadunund@openrobotics.org>
2023-04-27 08:59:20 +0800

    5.1.0
    Signed-off-by: Tom Noble <tom@rivelinrobotics.com>

--- sensor_msgs_py/setup.py
+++ sensor_msgs_py/setup.py
@@ -4,7 +4,7 @@ package_name = 'sensor_msgs_py'
 
 setup(
     name=package_name,
-    version='5.0.0',
+    version='5.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
